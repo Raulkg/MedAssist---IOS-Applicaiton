22dccfd9c0554db1d621d11d7e73afce2d9e8b5d
RahulKumar Gaddam <rgaddam@lcs-208-085.syr.edu>
2015-12-02 14:47:52 -0500

    Initial Commit

---
+++ MAP_FinalTests/MAP_FinalTests.swift
@@ -0,0 +1,36 @@
+//
+//  MAP_FinalTests.swift
+//  MAP_FinalTests
+//
+//  Created by RahulKumar Gaddam on 12/2/15.
+//  Copyright © 2015 RahulKumar Gaddam. All rights reserved.
+//
+
+import XCTest
+@testable import MAP_Final
+
+class MAP_FinalTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
