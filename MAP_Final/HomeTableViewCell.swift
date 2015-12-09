1ba9cb3678dd28af6f12b9ce4f0663233258617f
RahulKumar Gaddam <rgaddam@lcs-208-085.syr.edu>
2015-12-08 20:35:42 -0500

    workin Progress

---
+++ MAP_Final/HomeTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  HomeTableViewCell.swift
+//  MAP_Final
+//
+//  Created by RahulKumar Gaddam on 12/8/15.
+//  Copyright © 2015 RahulKumar Gaddam. All rights reserved.
+//
+
+import UIKit
+
+class HomeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
